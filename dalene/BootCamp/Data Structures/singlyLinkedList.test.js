ddf88ee033798cd99a823d44ab2c4dd78209a47a
Dalene Dews <ddewzy@gmail.com>
2021-01-22 16:00:01 -0800

    2021

--- dalene/BootCamp/Data Structures/singlyLinkedList.test.js
+++ dalene/BootCamp/Data Structures/singlyLinkedList.test.js
@@ -1,117 +1,117 @@
-const { test, expect } = require("@jest/globals");
-const { default: SinglyLinkedList } = require("./SinglyLinkedList");
+const {test, expect} = require('@jest/globals');
+const {default: SinglyLinkedList} = require('./SinglyLinkedList');
 
-test("can construct list", () => {
+test('can construct list', () => {
   const list = new SinglyLinkedList();
   expect(list).toBeDefined();
 });
 
-test("pop() when empty returns undefined", () => {
-  //Arrange
+test('pop() when empty returns undefined', () => {
+  // Arrange
   const list = new SinglyLinkedList();
 
-  //Act
+  // Act
   const result = list.pop();
 
-  //Assert
+  // Assert
   expect(result).toBeUndefined();
 });
 
-test("push an item into list", () => {
-  //Arrange
+test('push an item into list', () => {
+  // Arrange
   const list = new SinglyLinkedList();
   const value = 42;
 
-  //Act
+  // Act
   list.push(value);
 
-  //Assert
+  // Assert
   expect(list.length).toBe(1);
 });
 
-test("pop will return the last item pushed", () => {
-  //Arrange
+test('pop will return the last item pushed', () => {
+  // Arrange
   const list = new SinglyLinkedList();
   const value = 42;
   list.push(value);
 
-  //Act
+  // Act
   const result = list.pop();
 
-  //Assert
+  // Assert
   expect(list.length).toBe(0);
   expect(result).toBe(value);
 });
 
-test("unshift an item into list", () => {
-  //Arrange
+test('unshift an item into list', () => {
+  // Arrange
   const list = new SinglyLinkedList();
   const value = 42;
 
-  //Act
+  // Act
   list.push(value);
 
-  //Assert
+  // Assert
   expect(list.length).toBe(1);
 });
 
-test("shift will return the last item unshifted", () => {
-  //Arrange
+test('shift will return the last item unshifted', () => {
+  // Arrange
   const list = new SinglyLinkedList();
   const value = 42;
   list.unshift(value);
 
-  //Act
+  // Act
   const result = list.shift();
 
-  //Assert
+  // Assert
   expect(list.length).toBe(0);
   expect(result).toBe(value);
 });
 
-test("pop returns last item pushed, when multiple items", () => {
-  const first = 5,
-    second = 70;
+test('pop returns last item pushed, when multiple items', () => {
+  const first = 5;
+  const second = 70;
   const list = new SinglyLinkedList();
   list.push(first);
   list.push(second);
 
   const result = list.pop();
 
-  //Assert
+  // Assert
   expect(list.length).toBe(1);
   expect(result).toBe(second);
 });
 
-test("shift returns last item unshifted, when multiple items", () => {
-  const first = 5,
-    second = 70;
+test('shift returns last item unshifted, when multiple items', () => {
+  const first = 5;
+  const second = 70;
   const list = new SinglyLinkedList();
   list.unshift(first);
   list.unshift(second);
 
   const result = list.shift();
 
-  //Assert
+  // Assert
   expect(list.length).toBe(1);
   expect(result).toBe(second);
 });
 
-test("shift returns first item pushed", () => {
-  const first = 5,
-    second = 70;
+test('shift returns first item pushed', () => {
+  const first = 5;
+  const second = 70;
   const list = new SinglyLinkedList();
   list.push(first);
   list.push(second);
 
   const result = list.shift();
 
-  //Assert
+  // Assert
   expect(list.length).toBe(1);
   expect(result).toBe(first);
 });
 
-test("popping last item will reset list state to initial state", () => {
+test('popping last item will reset list state to initial state', () => {
   const list = new SinglyLinkedList();
   const first = 42;
   const second = 43;
@@ -125,7 +125,7 @@ test("popping last item will reset list state to initial state", () => {
   expect(result).toBe(second);
 });
 
-test("shifting last item will reset list state to initial state", () => {
+test('shifting last item will reset list state to initial state', () => {
   const list = new SinglyLinkedList();
   const first = 42;
   const second = 43;
